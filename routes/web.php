28979b1eac21b7f7cb916ec5c5231bcf4782ea9d
vickymaulana <vickymaulanna@gmail.com>
2024-03-28 21:32:20 +0700

    Refactor routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/blank-page', [App\Http\Controllers\HomeController::class, 'blank'])->name('blank');
 Route::middleware(['auth'])->group(function () {
-    Route::get('/pt', [App\Http\Controllers\PtController::class, 'index'])->name('pt.index');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/blank-page', [App\Http\Controllers\HomeController::class, 'blank'])->name('blank');
+
 });
-// Route::get('/pt', [PtController::class, 'index'])->name('pt.index');
