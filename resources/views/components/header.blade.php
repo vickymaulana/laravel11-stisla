edefd129e6e1d02ee4349dd0fe1eed7761702837
vickymaulana <vickymaulanna@gmail.com>
2024-09-15 16:03:38 +0700

    Refactor header and app layouts
    - Add sidebar toggle button to the left side of the header
    - Update user dropdown menu in the header to display a shorter name
    - Update logout link in the user dropdown menu to use the logout route
    - Update app layout to include a main content section
    - Update CSS and JS dependencies to use CDN links
    - Update font import links to use HTTPS
    - Update Bootstrap version to 4.5.2
    - Update Font Awesome version to 5.15.1
    - Update custom CSS files and add reverse skin CSS file
    - Update JS libraries and add custom JS files

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,36 +1,37 @@
 @auth
 <div class="navbar-bg"></div>
 <nav class="navbar navbar-expand-lg main-navbar">
+    <!-- Sidebar Toggle Button on the Left -->
+    <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg">
+        <i class="fas fa-bars"></i>
+    </a>
+
+    <!-- Right Side Navbar Items -->
     <ul class="navbar-nav ml-auto">
-        <li>
-            <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg">
-                <i class="fas fa-bars"></i>
-            </a>
-        </li>
         <li class="dropdown">
             <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                 <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">
-                    Hai, {{ auth()->check() ? substr(auth()->user()->name, 0, 10) : 'Tamu' }}
+                    Hai, {{ substr(auth()->user()->name, 0, 10) }}
                 </div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-title">
-                    Selamat Datang, {{ auth()->check() ? substr(auth()->user()->name, 0, 10) : 'Tamu' }}
+                    Selamat Datang, {{ substr(auth()->user()->name, 0, 10) }}
                 </div>
-                <a class="dropdown-item has-icon edit-profile" href="{{ route('profile.edit') }}" data-id="{{ \Auth::id() }}">
+                <a class="dropdown-item has-icon edit-profile" href="{{ route('profile.edit') }}">
                     <i class="fa fa-user"></i> Edit Profile
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="{{ url('logout') }}" class="dropdown-item has-icon text-danger"
-                   onclick="event.preventDefault(); localStorage.clear();  document.getElementById('logout-form').submit();">
+                <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
-                <form id="logout-form" action="{{ url('/logout') }}" method="POST" class="d-none">
-                    {{ csrf_field() }}
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
                 </form>
             </div>
         </li>
-        @endauth
     </ul>
 </nav>
+@endauth
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,32 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Meta Tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet"
-        href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('css/components.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('css/custom.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('css/skins/reverse.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Title -->
     <title>@yield('title') &mdash; Laravel - Stisla</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
+    <!-- Bootstrap CSS (Bootstrap 4) -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+    <!-- Custom CSS Files -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/skins/reverse.css') }}">
+
+    <!-- Additional CSS (if any) -->
+    @stack('css')
 </head>
 <body>
     <div id="app">
@@ -33,28 +36,34 @@
 
             <!-- Sidebar -->
             @include('components.sidebar')
-            
-            <!-- Content -->
-            @yield('content')
+
+            <!-- Main Content -->
+            <div class="main-content">
+                @yield('content')
+            </div>
 
             <!-- Footer -->
             @include('components.footer')
         </div>
     </div>
 
-    <!-- General JS Scripts -->
-    <script src="{{ asset('library/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('library/popper.js/dist/umd/popper.js') }}"></script>
-    <script src="{{ asset('library/tooltip.js/dist/umd/tooltip.js') }}"></script>
-    <script src="{{ asset('library/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- jQuery and Popper.js (required for Bootstrap 4) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Bootstrap JS (Bootstrap 4) -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Custom JS Libraries -->
     <script src="{{ asset('library/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
     <script src="{{ asset('library/moment/min/moment.min.js') }}"></script>
+
+    <!-- Template JS Files -->
     <script src="{{ asset('js/stisla.js') }}"></script>
-
-    @stack('scripts')
-
-    <!-- Template JS File -->
     <script src="{{ asset('js/scripts.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
+
+    <!-- Additional JS (if any) -->
+    @stack('scripts')
 </body>
 </html>
